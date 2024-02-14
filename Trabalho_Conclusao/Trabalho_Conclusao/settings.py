d5c879590ecde11dd1a44d5cc41e53c0c58f0f90
Vitória Mattos <qwertyviih@gmail.com>
2024-02-14 12:05:27 -0300

    feat: :zap: add login

--- Trabalho_Conclusao/Trabalho_Conclusao/settings.py
+++ Trabalho_Conclusao/Trabalho_Conclusao/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+# import os
+# import environ
+
+# Inicializando as variáveis de ambiente
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +49,8 @@ INSTALLED_APPS = [
     'perfil.apps.PerfilConfig',
     'registro.apps.RegistroConfig',
     'roteiro.apps.RoteiroConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +140,19 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy config
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Auth config
+LOGIN_URL = "users:login"
+
+# # Email config
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+# EMAIL_HOST = env("EMAIL_HOST")
+# EMAIL_PORT = env("EMAIL_PORT")
+# EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+# EMAIL_USE_TLS = True
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\anand\Desktop\trabalho\Trabalho-de-conclusao\venv
+set VIRTUAL_ENV=C:\Users\Anderson\OneDrive\Área de Trabalho\Trabalho-de-conclusao\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
